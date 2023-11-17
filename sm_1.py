b7bf92f1e2b4a0c52ddf1fc1f844512540e95818
llubyanskikh <147454826+llubyanskikh@users.noreply.github.com>
2023-11-17 16:04:54 +0500

    Delete sm_1.py

--- sm_1.py
+++
@@ -1,9 +0,0 @@
-user_input = input("Введите последовательность чисел, разделенных пробелом: ")
-
-
-numbers_list = [int(num) for num in user_input.split()]
-
-numbers_tuple = tuple(numbers_list)
-
-print("Список:", numbers_list)
-print("Кортеж:", numbers_tuple)
