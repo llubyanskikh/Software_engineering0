18ed2b1667b3dd8be2d4048b85734ffdddf673d7
llubyanskikh <147454826+llubyanskikh@users.noreply.github.com>
2023-11-17 18:54:54 +0500

    Add files via upload

---
+++ sm_1.py
@@ -0,0 +1,35 @@
+from collections import Counter
+import re
+
+
+def read_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content
+
+
+def count_words(text):
+    words = re.findall(r'\b\w+\b', text.lower())
+    return len(words)
+
+
+def find_most_common_word(text):
+    words = re.findall(r'\b\w+\b', text.lower())
+    word_counts = Counter(words)
+    most_common_word = word_counts.most_common(1)
+    return most_common_word[0][0] if most_common_word else None
+
+
+def main():
+    file_path = 'input.txt'  # Укажите имя вашего файла
+    article_content = read_file(file_path)
+
+    word_count = count_words(article_content)
+    most_common_word = find_most_common_word(article_content)
+
+    print(f"Количество слов в статье: {word_count}")
+    print(f"Самое часто встречающееся слово: {most_common_word}")
+
+
+if __name__ == "__main__":
+    main()
