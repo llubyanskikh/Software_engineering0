a37157c83a63eab4218fdbbcb39f5e015b01a189
llubyanskikh <147454826+llubyanskikh@users.noreply.github.com>
2023-11-24 02:54:31 +0500

    Update sm_2.py

--- sm_2.py
+++ sm_2.py
@@ -1,17 +1,16 @@
-# -*- coding: windows-1251 -*-
 def read_file(sm2):
     try:
         with open(sm2, 'r') as file:
             read_file = file.read()
             if not read_file:
-                raise Exception("Файл пустой")
+                raise Exception("Г”Г Г©Г« ГЇГіГ±ГІГ®Г©")
             else:
-                print("Содержимое файла:")
+                print("Г‘Г®Г¤ГҐГ°Г¦ГЁГ¬Г®ГҐ ГґГ Г©Г«Г :")
                 print(read_file)
     except FileNotFoundError:
-        print(f"Файл {sm2} не найден")
+        print(f"Г”Г Г©Г« {sm2} Г­ГҐ Г­Г Г©Г¤ГҐГ­")
     except Exception as e:
-        print(f"Ошибка: {e}")
+        print(f"ГЋГёГЁГЎГЄГ : {e}")
 
 read_file('sm2.txt')
 
